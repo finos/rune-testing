762537c72313af6f3c419c94dbe0dd49be7cbf24
hugohills-regnosys <hugo.hills@regnosys.com>
2023-04-18 16:25:59 +0100

    Add string equals to WhitespaceAgnosticAssert

--- src/main/java/com/regnosys/testing/WhitespaceAgnosticAssert.java
+++ src/main/java/com/regnosys/testing/WhitespaceAgnosticAssert.java
@@ -2,11 +2,16 @@ package com.regnosys.testing;
 
 import org.junit.jupiter.api.Assertions;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public class WhitespaceAgnosticAssert {
     public static void assertEquals(String s1, String s2) {
         Assertions.assertEquals(normalize(s1), normalize(s2));
+    }
+
+    public static boolean equals(String s1, String s2) {
+        return Objects.equals(normalize(s1), normalize(s2));
     }
 
     private static String normalize(String str) {
