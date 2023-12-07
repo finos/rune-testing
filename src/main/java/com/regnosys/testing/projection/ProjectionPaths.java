ca53c4dbc3058f614eb300c68ad2b01b411a76c7
hugohills-regnosys <hugo.hills@regnosys.com>
2023-12-07 15:23:45 +0000

    Add ProjectionPaths

---
+++ src/main/java/com/regnosys/testing/projection/ProjectionPaths.java
@@ -0,0 +1,30 @@
+package com.regnosys.testing.projection;
+
+import com.regnosys.rosetta.common.reports.RegReportPaths;
+import com.rosetta.model.lib.ModelReportId;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ProjectionPaths {
+
+    public static final Path PROJECTION_PATH = Paths.get("projection");
+    public static final String PROJECTION_EXPECTATIONS_FILE_NAME = "projection-expectations.json";
+
+    /**
+     * Projection input file is the reports output file (loaded from reports output directory).
+     */
+    public static Path getProjectionDataItemInputPath(ModelReportId reportIdentifier, String dataSetName, Path reportDataItemInputPath) {
+        Path reportOutputPath = RegReportPaths.getDefault().getOutputRelativePath();
+        return RegReportPaths.getReportExpectationFilePath(reportOutputPath, reportIdentifier, dataSetName, reportDataItemInputPath);
+    }
+
+    public static Path getProjectionDataItemOutputPath(Path projectionDataSetPath, Path projectionDataItemInputPath) {
+        String projectionDataItemOutputPath = toXmlFileExt(projectionDataItemInputPath.getFileName().toString());
+        return projectionDataSetPath.resolve(projectionDataItemOutputPath);
+    }
+
+    private static String toXmlFileExt(String path) {
+        return path.substring(0, path.lastIndexOf(".")) + ".xml";
+    }
+}
