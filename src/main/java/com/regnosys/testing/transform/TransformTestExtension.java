12f33d9e5c4cdc11a3919126616cf9bf06e958c9
Chuks-Ajeh <chuks.ajeh@regnosys.com>
2024-04-25 11:26:09 +0100

    task-4364: unused transform exceptions removed

--- src/main/java/com/regnosys/testing/transform/TransformTestExtension.java
+++ src/main/java/com/regnosys/testing/transform/TransformTestExtension.java
@@ -1,44 +1,23 @@
 package com.regnosys.testing.transform;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
-import com.google.common.io.Resources;
 import com.google.inject.Guice;
 import com.google.inject.Module;
 import com.regnosys.rosetta.common.hashing.ReferenceConfig;
-import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper;
-import com.regnosys.rosetta.common.serialisation.RosettaObjectMapperCreator;
-import com.regnosys.rosetta.common.transform.PipelineModel;
-import com.regnosys.rosetta.common.transform.TestPackModel;
 import com.regnosys.rosetta.common.validation.RosettaTypeValidator;
 import com.regnosys.testing.TestingExpectationUtil;
-import com.regnosys.testing.projection.ProjectionTestExtension;
 import com.regnosys.testing.reports.ExpectedAndActual;
 import com.regnosys.testing.reports.ReportTestExtension;
 import com.rosetta.model.lib.RosettaModelObject;
-import com.rosetta.model.lib.reports.Tabulator;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.params.provider.Arguments;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.SAXException;
 
 import javax.inject.Inject;
-import javax.xml.XMLConstants;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import javax.xml.validation.Validator;
-import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
