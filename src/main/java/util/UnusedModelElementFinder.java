1a643f5d0f7226a16ecdbd846733a5c9e449dbe4
PayalKhanna <payal.khanna@regnosys.com>
2024-05-31 12:45:30 +0100

    task-4522: regenerated header

--- src/main/java/util/UnusedModelElementFinder.java
+++ src/main/java/util/UnusedModelElementFinder.java
@@ -1,11 +1,11 @@
 package util;
 
 /*-
- * #%L
+ * ==============
  * Rosetta Testing
- * %%
+ * ==============
  * Copyright (C) 2022 - 2024 REGnosys
- * %%
+ * ==============
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +17,7 @@ package util;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * #L%
+ * ==============
  */
 
 import com.google.common.collect.Lists;
