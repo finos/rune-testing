bf0723d6f1a96bb6add1f49305eab97433d81a8d
Simon Cockx <simon.cockx@regnosys.com>
2024-08-21 12:08:26 +0200

    Updated

--- src/main/java/util/UnusedModelElementFinder.java
+++ src/main/java/util/UnusedModelElementFinder.java
@@ -29,13 +29,16 @@ import com.regnosys.rosetta.rosetta.simple.AnnotationRef;
 import com.regnosys.rosetta.rosetta.simple.Data;
 import com.regnosys.rosetta.rosetta.simple.Function;
 import com.regnosys.rosetta.transgest.ModelLoader;
+import com.regnosys.rosetta.utils.ModelIdProvider;
 import com.regnosys.testing.RosettaTestingInjectorProvider;
+import com.rosetta.model.lib.ModelSymbolId;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import java.util.*;
 
 /**
@@ -43,16 +46,19 @@ import java.util.*;
  */
 public class UnusedModelElementFinder {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UnusedModelElementFinder.class);
-    private final Set<String> listOfTypes = new HashSet<>();
-    private final Set<String> listOfUsedTypes = new HashSet<>();
-    private final Set<String> listOfOrphanedTypes = new HashSet<>();
+    private final ModelIdProvider modelIdProvider;
 
-    private final Set<String> listOfDeprecatedTypes = new HashSet<>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(UnusedModelElementFinder.class);
+    private final Set<ModelSymbolId> listOfTypes = new HashSet<>();
+    private final Set<ModelSymbolId> listOfUsedTypes = new HashSet<>();
+    private final Set<ModelSymbolId> listOfOrphanedTypes = new HashSet<>();
+
+    private final Set<ModelSymbolId> listOfDeprecatedTypes = new HashSet<>();
     private final List<RosettaModel> models;
 
-    public UnusedModelElementFinder(List<RosettaModel> models) {
+    public UnusedModelElementFinder(List<RosettaModel> models, ModelIdProvider modelIdProvider) {
         this.models = models;
+        this.modelIdProvider = modelIdProvider;
     }
 
     public static void main(String[] args) {
@@ -60,7 +66,7 @@ public class UnusedModelElementFinder {
         ModelLoader loader = injector.getInstance(ModelLoader.class);
         List<RosettaModel> models = loader.loadRosettaModels(ClassPathUtils.findRosettaFilePaths().stream().map(UrlUtils::toUrl));
 
-        new UnusedModelElementFinder(models).run();
+        new UnusedModelElementFinder(models, injector.getInstance(ModelIdProvider.class)).run();
     }
 
     public void run() {
@@ -79,10 +85,6 @@ public class UnusedModelElementFinder {
 
     }
 
-    private String getQualifiedName(RosettaType type) {
-        return type.getNamespace().getName() + "." + type.getName();
-    }
-
     private void generateTypesList() {
 
         for (RosettaModel model : models) {
@@ -92,10 +94,11 @@ public class UnusedModelElementFinder {
                     .filter(Data.class::isInstance)
                     .map(Data.class::cast)
                     .forEach(dataType -> {
-                        LOGGER.trace(" Processing data type: {}", getQualifiedName(dataType));
-                        listOfTypes.add(getQualifiedName(dataType));
+                        ModelSymbolId id = modelIdProvider.getSymbolId(dataType);
+                        LOGGER.trace(" Processing data type: {}", id);
+                        listOfTypes.add(id);
                         if(null!=dataType.getSuperType())
-                            listOfUsedTypes.add(getQualifiedName(dataType.getSuperType()));
+                            listOfUsedTypes.add(modelIdProvider.getSymbolId(dataType.getSuperType().getType()));
                         TreeIterator<EObject> eObjectTreeIterator = dataType.eAllContents();
                         updateUsedTypes(eObjectTreeIterator);
 
@@ -106,15 +109,17 @@ public class UnusedModelElementFinder {
                     .filter(RosettaEnumeration.class::isInstance)
                     .map(RosettaEnumeration.class::cast)
                     .forEach(enumeration -> {
-                        LOGGER.trace("Processing enumeration type {}", getQualifiedName(enumeration));
-                        listOfTypes.add(getQualifiedName(enumeration));
+                        ModelSymbolId id = modelIdProvider.getSymbolId(enumeration);
+                        LOGGER.trace("Processing enumeration type {}", id);
+                        listOfTypes.add(id);
                     });
 
             model.getElements().stream()
                     .filter(Function.class::isInstance)
                     .map(Function.class::cast)
                     .forEach(function -> {
-                        LOGGER.trace(" Processing function types {}.{}", function.getNamespace().getName(), function.getName());
+                        ModelSymbolId id = modelIdProvider.getSymbolId(function);
+                        LOGGER.trace(" Processing function {}", id);
                         // listOfTypes.add((function.getModel().getName().concat(".")).concat(function.getName()));
 
                         TreeIterator<EObject> eObjectTreeIterator = function.eAllContents();
@@ -130,7 +135,7 @@ public class UnusedModelElementFinder {
         annotations
                 .forEach(annotationRef -> {
                     if(annotationRef.getAnnotation().getName().equals("deprecated")){
-                        listOfDeprecatedTypes.add(getQualifiedName(dataType));
+                        listOfDeprecatedTypes.add(modelIdProvider.getSymbolId(dataType));
                     }
                 });
 
@@ -142,23 +147,23 @@ public class UnusedModelElementFinder {
         elements.stream()
                 .filter(TypeCall.class::isInstance)
                 .map(TypeCall.class::cast)
-                .forEach(typeCall -> listOfUsedTypes.add(getQualifiedName(typeCall.getType())));
+                .forEach(typeCall -> listOfUsedTypes.add(modelIdProvider.getSymbolId(typeCall.getType())));
 
     }
 
-    public Set<String> getListOfTypes() {
+    public Set<ModelSymbolId> getListOfTypes() {
         return listOfTypes;
     }
 
-    public Set<String> getListOfUsedTypes() {
+    public Set<ModelSymbolId> getListOfUsedTypes() {
         return listOfUsedTypes;
     }
 
-    public Set<String> getListOfOrphanedTypes() {
+    public Set<ModelSymbolId> getListOfOrphanedTypes() {
         return listOfOrphanedTypes;
     }
 
-    public Set<String> getListOfDeprecatedTypes() {
+    public Set<ModelSymbolId> getListOfDeprecatedTypes() {
         return listOfDeprecatedTypes;
     }
 }
--- src/test/java/util/UnusedModelElementFinderTest.java
+++ src/test/java/util/UnusedModelElementFinderTest.java
@@ -25,7 +25,9 @@ import com.regnosys.rosetta.common.util.ClassPathUtils;
 import com.regnosys.rosetta.common.util.UrlUtils;
 import com.regnosys.rosetta.rosetta.RosettaModel;
 import com.regnosys.rosetta.transgest.ModelLoader;
+import com.regnosys.rosetta.utils.ModelIdProvider;
 import com.regnosys.testing.RosettaTestingInjectorProvider;
+import com.rosetta.model.lib.ModelSymbolId;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.junit.jupiter.api.Test;
@@ -33,6 +35,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -42,40 +46,46 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class UnusedModelElementFinderTest {
     @Inject
     private ModelLoader modelLoader;
+    @Inject
+    private ModelIdProvider modelIdProvider;
 
     @Test
-    public void getlistOfOrphanedTypes() {
+    public void getListOfOrphanedTypes() {
 
         List<RosettaModel> models = modelLoader.loadRosettaModels(ClassPathUtils.findPathsFromClassPath(ImmutableList.of("model", "unused-model-element-finder"), ".*\\.rosetta", Optional.empty(), ClassPathUtils.class.getClassLoader()
                 ).stream()
                 .map(UrlUtils::toUrl));
-        UnusedModelElementFinder unusedModelElementFinder = new UnusedModelElementFinder(models);
+        UnusedModelElementFinder unusedModelElementFinder = new UnusedModelElementFinder(models, modelIdProvider);
 
         unusedModelElementFinder.run();
-        assertEquals(7, unusedModelElementFinder.getListOfTypes().size(), unusedModelElementFinder.getListOfTypes().toString());
+        Set<String> listOfTypes = unusedModelElementFinder.getListOfTypes().stream().map(ModelSymbolId::toString).collect(Collectors.toSet());
+        assertEquals(7, listOfTypes.size(), listOfTypes.toString());
 
-        assertTrue(unusedModelElementFinder.getListOfTypes().contains("cdm.test.Test1"), "ListOfTypes should contain cdm.test.Test1");
-        assertTrue(unusedModelElementFinder.getListOfTypes().contains("cdm.test.Test2"), "ListOfTypes should contain cdm.test.Test2");
-        assertTrue(unusedModelElementFinder.getListOfTypes().contains("cdm.test.Test3"),"ListOfTypes should contain cdm.test.Test3");
-        assertTrue(unusedModelElementFinder.getListOfTypes().contains("cdm.test.DirectionEnum"), "ListOfTypes should contain cdm.test.DirectionEnum");
-        assertTrue(unusedModelElementFinder.getListOfTypes().contains("cdm.test.TestEnum2Unused"), "ListOfTypes should contain cdm.test.TestEnum2Unused");
-        assertTrue(unusedModelElementFinder.getListOfTypes().contains("cdm.test.TestEnum3UsedInFuncOnly"), "ListOfTypes should contain cdm.test.TestEnum3UsedInFuncOnly");
+        assertTrue(listOfTypes.contains("cdm.test.Test1"), "ListOfTypes should contain cdm.test.Test1");
+        assertTrue(listOfTypes.contains("cdm.test.Test2"), "ListOfTypes should contain cdm.test.Test2");
+        assertTrue(listOfTypes.contains("cdm.test.Test3"),"ListOfTypes should contain cdm.test.Test3");
+        assertTrue(listOfTypes.contains("cdm.test.DirectionEnum"), "ListOfTypes should contain cdm.test.DirectionEnum");
+        assertTrue(listOfTypes.contains("cdm.test.TestEnum2Unused"), "ListOfTypes should contain cdm.test.TestEnum2Unused");
+        assertTrue(listOfTypes.contains("cdm.test.TestEnum3UsedInFuncOnly"), "ListOfTypes should contain cdm.test.TestEnum3UsedInFuncOnly");
 
-        assertEquals(5, unusedModelElementFinder.getListOfUsedTypes().size(), unusedModelElementFinder.getListOfUsedTypes().toString());
+        Set<String> listOfUsedTypes = unusedModelElementFinder.getListOfUsedTypes().stream().map(ModelSymbolId::toString).collect(Collectors.toSet());
+        assertEquals(5, listOfUsedTypes.size(), listOfUsedTypes.toString());
 
-        assertTrue(unusedModelElementFinder.getListOfUsedTypes().contains("cdm.test.Test1"), "ListOfUsedTypes should contain cdm.test.Test1");
-        assertTrue(unusedModelElementFinder.getListOfUsedTypes().contains("cdm.test.Test2"), "ListOfUsedTypes should contain cdm.test.Test2");
-        assertTrue(unusedModelElementFinder.getListOfUsedTypes().contains("cdm.test.Test3"), "ListOfUsedTypes should contain cdm.test.Test3");
-        assertTrue(unusedModelElementFinder.getListOfUsedTypes().contains("cdm.test.DirectionEnum"), "ListOfUsedTypes should contain cdm.test.DirectionEnum");
-        assertTrue(unusedModelElementFinder.getListOfUsedTypes().contains("cdm.test.TestEnum3UsedInFuncOnly"), "ListOfUsedTypes should contain cdm.test.TestEnum3UsedInFuncOnly");
+        assertTrue(listOfUsedTypes.contains("cdm.test.Test1"), "ListOfUsedTypes should contain cdm.test.Test1");
+        assertTrue(listOfUsedTypes.contains("cdm.test.Test2"), "ListOfUsedTypes should contain cdm.test.Test2");
+        assertTrue(listOfUsedTypes.contains("cdm.test.Test3"), "ListOfUsedTypes should contain cdm.test.Test3");
+        assertTrue(listOfUsedTypes.contains("cdm.test.DirectionEnum"), "ListOfUsedTypes should contain cdm.test.DirectionEnum");
+        assertTrue(listOfUsedTypes.contains("cdm.test.TestEnum3UsedInFuncOnly"), "ListOfUsedTypes should contain cdm.test.TestEnum3UsedInFuncOnly");
 
 
-        assertEquals(2, unusedModelElementFinder.getListOfOrphanedTypes().size(), unusedModelElementFinder.getListOfOrphanedTypes().toString());
-        assertTrue(unusedModelElementFinder.getListOfOrphanedTypes().contains("cdm.test.TestEnum2Unused"), "ListOfOrphanedTypes should contain cdm.test.TestEnum2Unused");
-        assertTrue(unusedModelElementFinder.getListOfOrphanedTypes().contains("cdm.test.Test4Unused"), "ListOfOrphanedTypes should contain cdm.test.Test4Unused");
+        Set<String> listOfOrphanedTypes = unusedModelElementFinder.getListOfOrphanedTypes().stream().map(ModelSymbolId::toString).collect(Collectors.toSet());
+        assertEquals(2, listOfOrphanedTypes.size(), listOfOrphanedTypes.toString());
+        assertTrue(listOfOrphanedTypes.contains("cdm.test.TestEnum2Unused"), "ListOfOrphanedTypes should contain cdm.test.TestEnum2Unused");
+        assertTrue(listOfOrphanedTypes.contains("cdm.test.Test4Unused"), "ListOfOrphanedTypes should contain cdm.test.Test4Unused");
 
-        assertEquals(1, unusedModelElementFinder.getListOfDeprecatedTypes().size(), unusedModelElementFinder.getListOfDeprecatedTypes().toString());
-        assertTrue(unusedModelElementFinder.getListOfDeprecatedTypes().contains("cdm.test.Test3"), "ListOfDeprecatedTypes should contain cdm.test.Test3");
+        Set<String> listOfDeprecatedTypes = unusedModelElementFinder.getListOfDeprecatedTypes().stream().map(ModelSymbolId::toString).collect(Collectors.toSet());
+        assertEquals(1, listOfDeprecatedTypes.size(), listOfDeprecatedTypes.toString());
+        assertTrue(listOfDeprecatedTypes.contains("cdm.test.Test3"), "ListOfDeprecatedTypes should contain cdm.test.Test3");
 
     }
 }
