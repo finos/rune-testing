849483327a9380b4df624f4ff84cd26906871e25
JayasriR <124573358+JayasriR@users.noreply.github.com>
2026-03-27 14:12:53 +0000

    Update Expected Checksum in LatestSchemesImportTest (#303)
    * update expected checksum in LatestSchemesImportTest
    
    * add new coding scheme XML files for commodity classification, collateral margin call response reasons, floating rate index, and commodity information providers
    
    * remove deprecated coding scheme XML files
    
    * update XML coding schemes to replace "ISDA" with "FpML" references

--- src/test/java/com/regnosys/testing/schemeimport/LatestSchemesImportTest.java
+++ src/test/java/com/regnosys/testing/schemeimport/LatestSchemesImportTest.java
@@ -62,7 +62,7 @@ public class LatestSchemesImportTest {
                 deleteFileFolder(new File(LatestSchemesImportTest.CODE_LIST_ZIP));
                 deleteFileFolder(new File(LatestSchemesImportTest.CODE_LIST));
             }
-            assertEquals("c8d253d3d7ef86d2c88e10cf8a2ce50a", checksum, "CodeList zip has been updated, run again with WRITE_EXPECTATIONS enabled then update expected checksum");
+            assertEquals("39a7fdb456d6c5b38272bd973fd0203a", checksum, "CodeList zip has been updated, run again with WRITE_EXPECTATIONS enabled then update expected checksum");
         }
     }
 
@@ -142,8 +142,7 @@ public class LatestSchemesImportTest {
                 System.out.println("copying " + path.toString());
                 Path d2 = destinationDir.resolve(path.getFileName());
                 System.out.println("destination File=" + d2);
-                if (Files.notExists(d2))
-                    Files.move(path, d2, StandardCopyOption.REPLACE_EXISTING);
+                Files.move(path, d2, StandardCopyOption.REPLACE_EXISTING);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
